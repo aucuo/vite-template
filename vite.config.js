62918f12fe9ecd55b41666f2728387158f136b61
aucuo <jahor.sykaviec@gmail.com>
2025-05-29 16:33:15 +0300

    new features: image optimizer, styles mixins and variables, reset.css

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { getHtmlFiles } from './config/utils.js'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     base: './',
     root: 'src',
+    publicDir: '../public',
 
     plugins: [
         tsconfigPaths(),
+        ViteImageOptimizer({
+            png: {
+                quality: 85,
+            },
+            jpeg: {
+                quality: 85,
+            },
+            jpg: {
+                quality: 85,
+            },
+            tiff: {
+                quality: 85,
+            },
+        }),
     ],
 
     server: {
